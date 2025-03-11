b9a176edb2511333ec63b491e557af21e1dac0ca
jvy-nn <mjapponce@mymail.mapua.edu.ph>
2025-03-11 17:19:04 +0800

    seed-model

---
+++ models/seed_class.py
@@ -0,0 +1,29 @@
+from typing import Optional,Dict
+from bson import ObjectId
+
+class SeedModel:
+    def __init__(self, name:str, 
+                 quantity: int,
+                 _id:Optional[ObjectId] = None):
+        self.name = name
+        self.quantity = quantity
+        self._id = _id
+    
+    def plant(self, quantity: int):
+        if quantity <= self.quantity:
+            self.quantity -= quantity
+
+    def to_dict(self) -> Dict:
+        return {
+            "_id": self._id,
+            "name":self.name,
+            "quantity": self.quantity
+        }
+    
+    @classmethod
+    def from_dict(cls, data: Dict):
+        return cls(
+            name = data['name'],
+            quantity = data['quantity'],
+            _id = data.get("_id")
+        )
